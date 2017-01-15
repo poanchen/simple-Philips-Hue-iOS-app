f0a960aa012cc66022aa47c336b848dd4b30ec6a
Chen Baron <bchen@node-1w7jr9ss8x9xo9hpnvfhp21rl.ipv6.telus.net>
2017-01-14 16:16:33 -0800

    Initial Commit

---
+++ hue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hue
+//
+//  Created by Chen, Baron on 1/14/17.
+//  Copyright © 2017 PoAn (Baron) Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
